735dbb7257a4f7fb01c78d48f965e6ae644bfe50
Riona-105 <rionafu1005@gmail.com>
2026-09-18 17:18:00 +0200

    defined inserr, update and delete

---
+++ dags/datawarehouse/data_modification.py
@@ -0,0 +1,87 @@
+import logging
+
+logger = logging.getlogger(__name__)
+table = "yt_api"
+
+
+def insert_rows(cur, conn, schema, row):
+    try:  # read data from json to staging area schema
+        if schema == 'staging':
+            video_id = 'video_id'
+
+            cur.execute(
+                f"""INSERT INYO {schema}.{table}("Video_ID", "Video_Title", "Upload_Date", "Duration", "Video_Type", "Video_Views", "Likes_Count", "Comments_Count")
+                VALUES (%(video_id)s, %(title)s, %(publishedAt)s, %(duration)s, %(viewCount)s, %(likeCount)s, %(commentCount)s);
+                """, row)
+
+        else:  # read data from previous layer to core schema
+            video_id = 'Video_ID'
+
+            cur.execute(
+                f"""INSERT INTO {schema}.{table}("Video_ID", "Video_Title", "Upload_Date", "Duration", "Video_Type", "Video_Views", "Likes_Count", "Comments_Count")
+                VALUES (%(Video_ID)s, %(Video_Title)s, %(Upload_Date)s, %(Duration)s, %(View_Type)s, %(Video_Views)s, %(Likes_Count)s, %(Comments_Count)s);
+                """, row,
+            )
+
+        conn.commit()
+        logger.info(f"Inserted row with Video_ID:{row[video_id]}")
+
+    except Exception as e:
+        logger.error(f"Error inserting row with Video_ID:{row[video_id]}")
+        raise e
+
+
+def update_rows(cur, conn, schema, row):
+    try:
+        # staging area
+        if schema == 'staging':
+            video_id = 'video_id'
+            upload_date = 'publishedAt'
+            video_title = 'title'
+            video_views = 'viewCount'
+            lieks_count = 'likeCount'
+            comments_count = 'commentCount'
+        # core
+        else:
+            video_id = 'Video_ID'
+            upload_date = 'Upload_Date'
+            video_title = 'Video_Title'
+            video_views = 'Video_Views'
+            likes_count = 'Likes_Count'
+            comments_count = 'Comments_Count'
+
+        cur.execute(
+            f"""
+            UPDATE {schema}.{table}
+            SET "Video_Title" = %({video_title})s,
+                "Video_Views" = %({video_views})s,
+                "Likes_Count" = %({likes_count})s,
+                "Comments_Count" = %({comments_count})s
+            WHERE "Video_ID" = %({video_id})s AND "Upload_Date" + %({upload_date})s;
+            """, row,)
+
+        conn.commit()
+        logger.info(f"Updated row with Video_ID:{row[video_id]}")
+
+    except Exception as e:
+        logger.error(f"Error updating row with Video_ID:{row[video_id]}")
+        raise e
+
+
+def delete_rows(cur, conn, schema, ids_to_delete):
+    try:  # convert variables into proper sql format string, using pythong string join method
+        ids_to_delete = f"""({','.join(f"'{id}'" for id in ids_to_delete)})"""
+
+        cur.execute(
+            f"""
+            DELETE FROM {schema}.{table}
+            WHERE "Video_ID" IN {ids_to_delete}
+            """
+        )
+
+        conn.commit()
+        logger.info(f"Deleted row with Video_ID:{ids_to_delete}")
+
+    except Exception as e:
+        logger.error(f"Error deleting row with Video_ID:{ids_to_delete}")
+        raise e
